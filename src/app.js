da71fb154b95906c322a496c913fd732bf829fc7
Victortxr87 <82127678+Victortxr87@users.noreply.github.com>
2023-05-24 20:49:55 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+require ('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+
+    res.send('Bem vindo a api do meu site pessoal');
+
+});
+
+app.listen(port, () => {
+
+    console.log(`Servidor rodando na porta ${port}`);
+
+});
+
