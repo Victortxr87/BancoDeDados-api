a8dc8d466db54516a72adb1da2d3daae04985ec7
Victortxr87 <82127678+Victortxr87@users.noreply.github.com>
2023-06-04 20:06:58 +0100

    adc o express-async-errors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 require ('./config/dotenv')
+require ('express-async-errors')
 
 const express = require('express');
 const {initDatabase } = require('./config/db');
@@ -30,6 +31,11 @@ app.use('/api/informacoes', informacoesRoute);
 app.use('/api/auth', authRoute);
 
 initDatabase();
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado!');
+});
 
 app.listen(port, () => {
 
